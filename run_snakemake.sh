7704df0d6901dc0932dfa1cae32fb9a2069676c6
slsevilla <slsevilla@gmail.com>
2020-12-01 11:11:03 -0500

    update timestamp, handle output dir errors

--- run_snakemake.sh
+++ run_snakemake.sh
@@ -1,5 +1,6 @@
-test=$1
+pipeline=$1
 
+#handle yaml file
 parse_yaml() {
    local prefix=$2
    local s='[[:space:]]*' w='[a-zA-Z0-9_]*' fs=$(echo @|tr @ '\034')
@@ -16,41 +17,45 @@ parse_yaml() {
    }'
 }
 
-# read yaml file
 eval $(parse_yaml config/snakemake_config.yaml "config_")
 
 # set timestamp
-now=`date +"%Y%m%d_%H%M"`
+log_time=`date +"%Y%m%d_%H%M"`
+s_time=`date +"%Y%m%d_%H%M%S"`
 
-if [[ $test = "run" ]]; then
+#clean config_output_dir
+output_dir=${config_output_dir%/}
+
+#run pipeline, test pipeline, unlock pipeline
+if [[ $pipeline = "run" ]]; then
 
   #create log dir
-  if [ -d "${config_out_dir}/log" ]
+  if [ -d "${output_dir}/log" ]
   then
     echo
     echo "Pipeline re-run, jobid:"
   else
-    mkdir "${config_out_dir}/log"
+    mkdir "${output_dir}/log"
     echo
     echo "Pipeline initial run, jobid:"
   fi
 
   # copy config inputs for ref
-  files_save=('config/snakemake_config.yaml' 'config/cluster_config.yml' 'config/cluster_config.yml' ${config_multiplex_manifest} ${config_sample_manifest})
+  files_save=('config/snakemake_config.yaml' 'config/cluster_config.yml' 'config/index_config.yaml' ${config_multiplex_manifest} ${config_sample_manifest})
 
   for f in ${files_save[@]}; do
     IFS='/' read -r -a strarr <<< "$f"
-    cp $f "${config_out_dir}/log/${now}_${strarr[-1]}"
+    cp $f "${output_dir}/log/${log_time}_${strarr[-1]}"
   done
 
   #submit job to cluster
   sbatch --job-name="iCLIP" --gres=lscratch:200 --time=120:00:00 --mail-type=BEGIN,END,FAIL \
   snakemake --latency-wait 120  -s workflow/Snakefile --printshellcmds --cluster-config config/cluster_config.yml --keep-going \
   --restart-times 1 --cluster "sbatch --gres {cluster.gres} --cpus-per-task {cluster.threads} -p {cluster.partition} -t {cluster.time} \
-  --mem {cluster.mem} --cores {cluster.cores} --job-name={params.rname} --output=${config_out_dir}/log/{params.rname}_${now}.out" -j 500 --rerun-incomplete
+  --mem {cluster.mem} --cores {cluster.cores} --job-name={params.rname} --output=${output_dir}/log/${s_time}_{params.rname}.out" -j 500 --rerun-incomplete
 
-elif [[ $test = "unlock" ]]; then
-  snakemake -s workflow/Snakefile --unlock --cores=8
+elif [[ $pipeline = "unlock" ]]; then
+  snakemake -s workflow/Snakefile --unlock --cores 8
 else
   #run snakemake
   snakemake -s workflow/Snakefile -npr
