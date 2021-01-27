a6c1fbe2a0a17a94383a4b7d247b81f77c3b2947
slsevilla <slsevilla@gmail.com>
2021-01-27 12:13:03 -0500

    increase length bins

--- workflow/scripts/04_alignment_stats.R
+++ workflow/scripts/04_alignment_stats.R
@@ -16,10 +16,11 @@ Seq_Processing <- function(txt_in,cat){
   dat = data.frame(len=scan(txt_in))
   
   # set up cut-off values 
-  breaks <- c(0,10,20,30,40,50,60,70,80,90)
+  breaks <- c(0,15,30,45,60,75,90,105,120,135,150,165)
   
   # specify interval/bin labels
-  tags <- c("[0-10)","[10-20)", "[20-30)", "[30-40)", "[40-50)", "[50-60)","[60-70)", "[70-80)","[80-90)")
+  tags <- c("[0-15)","[15-30)", "[30-45)", "[45-60)", "[60-75)", "[75-90)","[90-105)", "[105-120)","[120-135)",
+            "[135-150)","[150-165)")
   
   # bucketing values into bins
   group_tags <- cut(dat$len, 
@@ -49,109 +50,7 @@ Seq_Processing <- function(txt_in,cat){
   close(fileConn)
 }
 
-
-
-
-
-
-##testing
-sampleid = "WT_fCLIP"
-bam_aligned = "/Volumes/data/iCLIP/marco/fCLIP_HS/00_qc_post/WT_fCLIP_aligned.bam"
-bam_unaligned = "/Volumes/data/iCLIP/marco/fCLIP_HS/00_qc_post/WT_fCLIP_unaligned.bam"
-output_dir = "/Volumes/sevillas2/git/iCLIP/testing/"
-bam_file=bam_aligned
-cat="aligned"
-
-BamProcessing <- function(bam_file,cat){
-  #Read in bam
-  bam <- scanBam(bam_file,index=bam_file)
-  
-  #aligned workflow
-  if(cat=="aligned"){
-    
-    #Convert seq to df, create length counts
-    seq_df <- data.frame(x=bam[[1]]$qwidth,len = bam[[1]]$qwidth)
-    remove(bam)
-    
-    # set up cut-off values 
-    breaks <- c(0,10,20,30,40,50,60,70,80,90)
-    
-    # specify interval/bin labels
-    tags <- c("[0-10)","[10-20)", "[20-30)", "[30-40)", "[40-50)", "[50-60)","[60-70)", "[70-80)","[80-90)")
-    
-    # bucketing values into bins
-    group_tags <- cut(seq_df$len, 
-                      breaks=breaks, 
-                      include.lowest=TRUE, 
-                      right=FALSE, 
-                      labels=tags)
-    
-    #Create and save histogram of lengths
-    p = ggplot(data = as_tibble(group_tags), mapping = aes(x=value)) + 
-      geom_bar(fill="blue",color="white",alpha=0.7) + 
-      stat_count(geom="text", aes(label=..count..), vjust=-0.5) +
-      xlab('Sequence Length') + ylab('Number of Reads') +
-      theme_minimal()
-    p_final = p + ggtitle(paste(cat,"sequence lengths:\n",sampleid))
-    
-    file_save = paste(output_dir,sampleid,"_",cat,".png",sep="")
-    ggsave(file_save,p_final)
-    
-    #Create and save text file summary 
-    file_save = paste(output_dir,sampleid,"_",cat,".txt",sep="")
-    fileConn<-file(file_save)
-    line1 = paste("\n* SampleID: ",sampleid,"\n\n")
-    line2 = paste("\t + Number of ",cat," reads: ",length(bam[[1]]$qwidth),"\n",sep="")
-    line3 = paste("\t + Average read length (std): ",format(round(mean(bam[[1]]$qwidth),2),nsmall = 2), 
-                  " (", format(round(sd(bam[[1]]$qwidth),2),nsmall = 2),")\n",sep="")
-    
-    writeLines(paste(line1,line2,line3,sep=""), fileConn)
-    close(fileConn)
-    
-  }
-  #unaligned workflow
-  else {
-    #Convert seq to df, create length counts
-    seq_df <- data.frame(x=bam[[1]]$seq,len = nchar(bam[[1]]$seq))
-    remove(bam)
-    
-    # set up cut-off values 
-    breaks <- c(0,10,20,30,40,50,60,70,80,90)
-    
-    # specify interval/bin labels
-    tags <- c("[0-10)","[10-20)", "[20-30)", "[30-40)", "[40-50)", "[50-60)","[60-70)", "[70-80)","[80-90)")
-    
-    # bucketing values into bins
-    group_tags <- cut(seq_df$len, 
-                      breaks=breaks, 
-                      include.lowest=TRUE, 
-                      right=FALSE, 
-                      labels=tags)
-    
-    #Create and save histogram of lengths
-    p = ggplot(data = as_tibble(group_tags), mapping = aes(x=value)) + 
-      geom_bar(fill="blue",color="white",alpha=0.7) + 
-      stat_count(geom="text", aes(label=..count..), vjust=-0.5) +
-      xlab('Sequence Length') + ylab('Number of Reads') +
-      theme_minimal()
-    p_final = p + ggtitle(paste(cat,"sequence lengths:\n",sampleid))
-
-    file_save = paste(output_dir,sampleid,"_",cat,".png",sep="")
-    ggsave(file_save,p_final)
-    
-    #Create and save text file summary 
-    file_save = paste(output_dir,sampleid,"_",cat,".txt",sep="")
-    fileConn<-file(file_save)
-    line1 = paste("\n* SampleID:",sampleid,"\n\n")
-    line2 = paste("\t + Number of ",cat," reads: ",nrow(seq_df),"\n",sep="")
-    line3 = paste("\t + Average read length (std): ",format(round(mean(seq_df$len),2),nsmall = 2), 
-                  " (", format(round(sd(seq_df$len),2),nsmall = 2),")\n",sep="")
-    writeLines(paste(line1,line2,line3,sep=""), fileConn)
-    close(fileConn)
-  }
-}
-
 # MAIN CODE
 #process unaligned, aligned  
-system.time(BamProcessing(bam_unaligned,"unaligned"))
-system.time(BamProcessing(bam_aligned,"aligned"))
+Seq_Processing(bam_unaligned,"unaligned")
+Seq_Processing(bam_aligned,"aligned")
