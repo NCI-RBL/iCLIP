c6819302ce420b27e20e604c07dcf775f22bb138
skchronicles <kuhnsa2@vcu.edu>
2022-01-06 18:03:20 -0500

    Refactoring MAPQ correction script to use less memory

--- workflow/scripts/06_correct_mapq.py
+++ workflow/scripts/06_correct_mapq.py
@@ -1,91 +1,25 @@
-# Author: Vishal Koparde, PhD
-# Date: Apr 2021
-import pysam
-import sys
-import argparse
-import os
-class Readinfo:
-	def __init__(self,read,save_read=False):
-		def _get_bitflag(r):
-			bitflag=str(r).split("\t")[1]
-			return str(bitflag)
-		self.bitflag=_get_bitflag(read)
-		self.mapq=int(read.mapping_quality)
-		self.nhtag=str(read.get_tag("NH"))
-		self.ref=read.reference_name
-		self.cigar=read.cigarstring
-		self.pos=read.reference_start
-		if save_read:
-			self.alignment=read
-		else:
-			self.alignment=""
-		self.refpos=list()
+#!/usr/bin/env python
 
-	def __str__(self):
-		return "%s\t%d\t%s\t%s\t%s\t%s"%(self.bitflag,self.mapq,self.nhtag,self.cigar,self.ref,self.pos)
-		
-	def is_spliced(self):
-		cigart=self.alignment.cigartuples
-		if 3 in list(map(lambda z:z[0],cigart)):
-			return True
-		else:
-			return False
-	
-	def get_reference_positions(self):
-		self.refpos=set(filter(lambda x:x!=None,self.alignment.get_reference_positions(full_length=True)))
-		
-def read_bam(bamfilename,save_alignments=False):
-	print("Reading BAM:%s"%(bamfilename))
-	bam=pysam.AlignmentFile(bamfilename, "rb")
-	bigdict = dict()
+"""
+@USAGE: 
+python correct_mapq.py \\
+    --inputBAM1 unaware.bam \\
+    --inputBAM2 aware_exon_masked_genome+transcriptome.bam \\
+    --inputBAM3 aware_unmaked_genome+transcriptome.bam \\
+    --outBAM  corrected_aware_unmaked_genome+transcriptome.bam \\
+    --outLOG  out.log
 
-	count=0
-	for read in bam.fetch():
-		count+=1
-		if count%1000000 == 0:
-			print("%d reads read from file: %s!"%(count,bamfilename))
-		qn=read.query_name
-		if not qn in bigdict:
-			bigdict[qn]=dict()
-			bigdict[qn]['reads']=list()
-			bigdict[qn]['maxmapq']=-1
-		bigdict[qn]['reads'].append(Readinfo(read,save_alignments))
-		if int(read.mapping_quality) > bigdict[qn]['maxmapq']:
-			bigdict[qn]['maxmapq']=int(read.mapping_quality)
-	bam.close()
-	return bigdict
-
-
-def find_overlapping_read_mapq(read,readlist):
-	mapq=-1
-	refpos=set(filter(lambda x:x!=None,read.alignment.get_reference_positions(full_length=True)))
-	for r in readlist:
-		if r.ref != read.ref:
-			continue
-		refpos1=set(filter(lambda x:x!=None,r.alignment.get_reference_positions(full_length=True)))
-		if len(refpos1.intersection(refpos))>=5:
-			mapq=r.alignment.mapping_quality
-			break
-	return mapq
-		
-
-def main():
-	parser = argparse.ArgumentParser(
-formatter_class=argparse.RawDescriptionHelpFormatter,
-description='''
-Inputs:
-
+@INPUTS:
 1. inputBAM1 --> Novoalign bam file (splicing unaware) with genome as reference
 2. inputBAM2 --> Novoalign bam file (splicing aware) with exon-masked genome + transcriptome as reference
 3. inputBAM3 --> Novoalign bam file (splicing aware) with unmasked genome + transcriptome as reference
 
 For inputBAM2 and inputBAM3, transcriptome alignments have already been converted back to genomic coordinates
 
-Outputs:
-
+@OUTPUTS:
 1. outBAM --> output BAM with corrected MAPQ scores. This uses inputBAM3 as a template for header etc.
-2. outTSV --> output with read-by-read mapping metadata eg.
-3. outLog --> 
+2. outTSV --> dummy output with read-by-read mapping metadata eg.
+3. outLog --> output log file containing a description of MAPQ changes
 
 *******
 NS500326:331:H53GGBGX5:2:21207:8400:7107:rbc:AGTAT
@@ -112,159 +46,279 @@ The recommended way to run this script is something like this:
 
 If the input BAMs are large with multimappings, then this script does tend to use significantly large amount of memory.
 Using --partition=largemem with 1TB of memory request to slurm is recommended.
-''')
-	parser.add_argument('--inputBAM1', dest='inputBAM1', type=str, required=True,
-						help='input BAM file1 (A.bam)')
-	parser.add_argument('--inputBAM2', dest='inputBAM2', type=str, required=True,
-						help='input BAM file2 (B.bam)')
-	parser.add_argument('--inputBAM3', dest='inputBAM3', type=str, required=True,
-						help='input BAM file3 (C.bam)')
-	parser.add_argument('--outBAM', dest='outBAM', type=str, required=True,
-						help='output BAM file (out.bam)')
-	parser.add_argument('--outTSV', dest='outTSV', type=str, required=True,
-						help='outTSV with read-by-read metadata')
-	parser.add_argument('--outLOG', dest='outLOG', type=str, required=True,
-						help='outLOG with mapq changes')
-	args = parser.parse_args()
+"""
 
-	bigdict1 = read_bam(args.inputBAM1,True)
-	bigdict2 = read_bam(args.inputBAM2,True)
-	bigdict3 = read_bam(args.inputBAM3,True)
-	print("Done reading BAMs!")
+# Python Standard Library
+from __future__ import print_function
+import sys
+
+# 3rd party imports from pypi
+import pysam
+import argparse
 
 
-	keys=[]
-	keys.extend(bigdict1.keys())
-	keys.extend(bigdict2.keys())
-	keys.extend(bigdict3.keys())
-	keys=set(keys)
-	dummy="%d\t%d\t%d\t%d\t%d\t%d"%(-1,-1,-1,-1,-1,-1)
-	o=open(args.outTSV,'w')
-	for key in keys:
-		o.write("%s\n"%(key))
-		if key in bigdict1:
-			o.write("File1:maxmapq:%d\n"%(bigdict1[key]['maxmapq']))
-			for i in bigdict1[key]['reads']:
-				o.write("%s\n"%(i))
-		else:
-			bigdict1[key]=dict()
-			bigdict1[key]['reads']=list()
-			bigdict1[key]['maxmapq']=-1
-			o.write("%s\n"%(dummy))
-		if key in bigdict2:
-			o.write("File2:maxmapq:%d\n"%(bigdict2[key]['maxmapq']))
-			for i in bigdict2[key]['reads']:
-				o.write("%s\n"%(i))
-		else:
-			bigdict2[key]=dict()
-			bigdict2[key]['reads']=list()
-			bigdict2[key]['maxmapq']=-1
-			o.write("%s\n"%(dummy))
-		if key in bigdict3:
-			o.write("File3:maxmapq:%d\n"%(bigdict3[key]['maxmapq']))
-			for i in bigdict3[key]['reads']:
-				o.write("%s\n"%(i))
-		else:
-			bigdict3[key]=dict()
-			bigdict3[key]['reads']=list()
-			bigdict3[key]['maxmapq']=-1
-			o.write("%s\n"%(dummy))
-		o.write("\n")
-	o.close()
-	
-	print("Done writing %s!"%(args.outTSV))
+def err(*message, **kwargs):
+    """Prints any provided args to standard error.
+    kwargs can be provided to modify print functions 
+    behavior.
+    @param message <any>:
+        Values printed to standard error
+    @params kwargs <print()>
+        Key words to modify print function behavior
+    """
+    print(*message, file=sys.stderr, **kwargs)
 
-	inbam = pysam.AlignmentFile(args.inputBAM3, "rb" )
-	outbam = pysam.AlignmentFile(args.outBAM, "wb", template=inbam )
-	inbam.close()
-	
-	o=open(args.outLOG,'w')
-	for rid in bigdict3.keys():
-		if bigdict3[rid]['maxmapq']<=0 and bigdict1[rid]['maxmapq']<=0 and bigdict2[rid]['maxmapq']<=0: # read is multimapped and all MAPQs are zero OR maxmapq is -1 ... readid absent in other BAM file(s)
-			for r in bigdict3[rid]['reads']:
-				outbam.write(r.alignment)
-		elif bigdict1[rid]['maxmapq']!=-1 and bigdict2[rid]['maxmapq']!=-1 and bigdict3[rid]['maxmapq']!=-1: # readid present in all 3 files
-			if (bigdict1[rid]['maxmapq'] > bigdict3[rid]['maxmapq']) or (bigdict2[rid]['maxmapq'] > bigdict3[rid]['maxmapq']): # better MAPQ may be available
-				spliced_reads=[]
-				for i,r in enumerate(bigdict3[rid]['reads']):
-					if not r.is_spliced:
-						outbam.write(r)
-					else:
-						spliced_reads.append(i)
-				for i in spliced_reads:
-					read=bigdict3[rid]['reads'][i]
-					mq3=read.mapq
-					mq1=mq2=-1
-					if bigdict1[rid]['maxmapq']>mq3: # file1 MAY have higher quality overlap
-						mq1=find_overlapping_read_mapq(read,bigdict1[rid]['reads'])
-					if bigdict2[rid]['maxmapq']>mq3: # file2 MAY have higher quality overlap
-						mq2=find_overlapping_read_mapq(read,bigdict2[rid]['reads'])
-					newmq=mq3
-					if mq1>newmq:
-						newmq=mq1
-					if mq2>newmq:
-						newmq=mq2
-					read.alignment.mapping_quality=newmq
-					if newmq!=mq3:
-						o.write("MAPQ changed from %d to %d :%s\n"%(mq3,newmq,read.alignment.query_name))
-					outbam.write(read.alignment)
-			else:
-				for r in bigdict3[rid]['reads']:
-					outbam.write(r.alignment)
-		elif bigdict3[rid]['maxmapq']!=-1 and bigdict2[rid]['maxmapq']!=-1 and bigdict1[rid]['maxmapq']==-1: # readid absent in first file
-			if bigdict2[rid]['maxmapq'] > bigdict3[rid]['maxmapq']:
-				spliced_reads=[]
-				for i,r in enumerate(bigdict3[rid]['reads']):
-					if not r.is_spliced:
-						outbam.write(r.alignment)
-					else:
-						spliced_reads.append(i)
-				for i in spliced_reads:
-					read=bigdict3[rid]['reads'][i]
-					mq3=read.mapq
-					mq2=-1
-					if bigdict2[rid]['maxmapq']>mq3: # file2 MAY have higher quality overlap
-						mq2=find_overlapping_read_mapq(read,bigdict2[rid]['reads'])
-					newmq=mq3
-					if mq2>newmq:
-						newmq=mq2
-					read.alignment.mapping_quality=newmq
-					if newmq!=mq3:
-						o.write("MAPQ changed from %d to %d :%s\n"%(mq3,newmq,read.alignment.query_name))
-					outbam.write(read.alignment)				
-			else:
-				for r in bigdict3[rid]['reads']:
-					outbam.write(r.alignment)
-		elif bigdict3[rid]['maxmapq']!=-1 and bigdict1[rid]['maxmapq']!=-1 and bigdict2[rid]['maxmapq']==-1: # readid absent in second file
-			if bigdict1[rid]['maxmapq'] > bigdict3[rid]['maxmapq']:
-				spliced_reads=[]
-				for i,r in enumerate(bigdict3[rid]['reads']):
-					if not r.is_spliced:
-						outbam.write(r)
-					else:
-						spliced_reads.append(i)
-				for i in spliced_reads:
-					read=bigdict3[rid]['reads'][i]
-					mq3=read.mapq
-					mq1=-1
-					if bigdict1[rid]['maxmapq']>mq3: # file2 MAY have higher quality overlap
-						mq1=find_overlapping_read_mapq(read,bigdict2[rid]['reads'])
-					newmq=mq3
-					if mq1>newmq:
-						newmq=mq1
-					read.alignment.mapping_quality=newmq
-					if newmq!=mq3:
-						o.write("MAPQ changed from %d to %d :%s\n"%(mq3,newmq,read.alignment.query_name))
-					outbam.write(read.alignment)				
-			else:
-				for r in bigdict3[rid]['reads']:
-					outbam.write(r.alignment)
-		else: # readid absent in first and second file
-			for r in bigdict3[rid]['reads']:
-				outbam.write(r.alignment)
-					
-	outbam.close()
-	o.close()
 
-if __name__ == "__main__":
-    main()
+def fatal(*message, **kwargs):
+    """Prints any provided args to standard error
+    and exits with an exit code of 1.
+    @param message <any>:
+        Values printed to standard error
+    @params kwargs <print()>
+        Key words to modify print function behavior
+    """
+    err(*message, **kwargs)
+    sys.exit(1)
+
+
+def collect_args():
+    """Parse and collect command-line options."""
+    parser = argparse.ArgumentParser(description='Reset unique alignment tags')
+    # Splicing-unaware with genome
+    # as reference
+    parser.add_argument(
+        '--inputBAM1', 
+        dest = 'inputBAM1', 
+        type = str, 
+        required = True, 
+        help = 'Input splicing-unaware genomic ' 
+               'SAM/BAM file'
+    )
+    # Splicing-aware with exon-masked 
+    # genome + transcriptome
+    parser.add_argument(
+        '--inputBAM2', 
+        dest = 'inputBAM2', 
+        type = str, 
+        required = True, 
+        help = 'Input splicing-aware, (exon-masked) '
+               'genome + transcriptome SAM/BAM file '
+    )
+    # Splicing-aware with unmasked 
+    # genome + transcriptome
+    parser.add_argument(
+        '--inputBAM3', 
+        dest = 'inputBAM3', 
+        type = str, 
+        required = True, 
+        help = 'Input splicing-aware, unmasked '
+               'genome + transcriptome SAM/BAM file'
+    )
+    # MAPQ corrected splicing-aware 
+    # with unmasked genome + transcriptome
+    parser.add_argument(
+		'--outBAM', 
+		dest = 'outBAM',
+		type = str, 
+		required = True,
+        help='Output MAPQ corrected splicing-aware, ' 
+             'unmasked genome + transcriptome SAM/BAM file'
+	)
+    # Output log file containing a 
+    # description of MAPQ changes
+    parser.add_argument(
+		'--outLOG', 
+		dest = 'outLOG',
+		type = str, 
+		required = True,
+        help = 'Output Log file containing description of '
+               'MAPQ changes with before and after values '
+	)
+    # TODO: Remove this option later,
+    # It was kept to ensure cross-
+    # compatibility between the 
+    # older script and this new 
+    # script, does not do anything!
+    parser.add_argument(
+		'--outTSV', 
+		dest = 'outTSV',
+		type = str, 
+		required = True,
+        help = 'Placeholder old output TSV file option'
+	)
+
+    parsed_args = parser.parse_args()
+    return parsed_args 
+
+
+def file_mode(file):
+    """Returns the appropriate bit for reading and writing 
+    SAM/BAM files. If file endswith(.bam) return a b for 
+    binary mode reading/writing required for bam files.
+    """
+    m = ''
+    if file.endswith('.bam'):
+        # Use binary BAM 
+        # I/O mode bit
+        m = 'b'
+    return m
+
+
+def get_mapq(file):
+    """Reads in a SAM/BAM file and stores the maximum MAPQ score
+    of a given read ID. Returns a dictionary where each key is a 
+    read ID and its value is set to its max MAPQ score, i.e.
+    dict['READ_ID'] = READ_ID_MAX_MAPQ where dict[str] = int.
+    """
+    # Dictionary to store each
+    # read IDs max MAPQ score
+    r2q = {}
+
+    # Set correct mode to work 
+	# with SAM or BAM files
+    mode = "r{}".format(file_mode(file))
+    
+    # Create input file handles 
+    # for reading in SAM/BAM file
+    infh = pysam.AlignmentFile(file, mode, check_header=False, check_sq=False)
+    
+    # Find max MAPQ of each read
+    for read in infh.fetch(until_eof=True):
+        rid = read.query_name
+        if rid not in r2q:
+            # Read ID encountered for the
+            # first time, add it 
+            r2q[rid] = int(read.mapping_quality)
+        else:
+            # Update the max quality score
+            # if another read with the same 
+            # read ID is encountered 
+            r2q[rid] = max(int(read.mapping_quality), r2q[rid])    
+    
+    infh.close()
+
+    return r2q
+
+
+def max_mapq(current_mapq, rid, lookup):
+    """Compares the current MAPQ score of a read to the MAPQ 
+    score in the reference lookup, and returns the max MAPQ 
+    score of the two values. The time complexity of 'key' in
+    'dict' is O(1) in python. 
+    """
+    if rid in lookup:
+        # Read ID in lookup,
+        # compare the current 
+        # MAPQ to other MAPQ
+        candidate_mapq = int(lookup[rid])
+        current_mapq = max(current_mapq, candidate_mapq)
+
+    return current_mapq
+
+
+def is_spliced(read):
+    """Check if a read is spliced by looking its cigar score 
+    If the read is spliced then the cigar score will contain 
+    a 3. Returns True if spliced and False if unspliced.
+    """
+    spliced = False
+    if 3 in list(map(lambda z:z[0],read.cigartuples)):
+        # Check if the first value 
+        # in a list of tuples is 
+        # equal to 3 or the N 
+        # operation (BAM_CREF_SKIP)
+        spliced = True
+    
+    return spliced
+
+
+def main():
+    """Collects command line arguments and corrects the MAPQ
+    scores in the splicing-aware, unmaked genome + transcriptome
+    (BAM3) using information in the splicing-unaware (BAM1) AND 
+    the splicing-aware (BAM2) files. In BAM3, each read is checked
+    to see if it is spliced or unspliced, if it is unspliced then 
+    it uses the MAPQ scores in the (BAM1); else, it uses it uses 
+    the spliced MAPQ scores (BAM2). 
+    """
+
+    # Parse command-line args 
+    args = collect_args()
+    
+    # Splicing-unaware, genomic reference
+    bamf1 = args.inputBAM1
+    # Splicing-aware, 
+    # exon-masked genome + transcriptome 
+    bamf2 = args.inputBAM2
+    # Splicing-aware, 
+    # unmaked genome + transcriptome
+    bamf3 = args.inputBAM3
+
+    # MAPQ corrected splicing-aware, 
+    # unmaked_genome + transcriptome
+    outbam = args.outBAM
+    # Log containing before and after 
+    # description of MAPQ value changes
+    outlog = args.outLOG
+
+    # Correct MAPQ
+    # Get MAPQs of unspliced and 
+    # spliced SAM/BAM files
+    unspliced_mapqs = get_mapq(bamf1)
+    spliced_mapqs   = get_mapq(bamf2)
+    # Create input file handles for read
+    # and writing of input and corrected 
+    # MAPQ output 
+    infh = pysam.AlignmentFile(
+        bamf3, "r{}".format(file_mode(bamf3)), 
+        check_header=False, 
+        check_sq=False
+    )
+    outfh = pysam.AlignmentFile(
+        outbam, "w{}".format(file_mode(outbam)), 
+        template=infh
+    )
+    # Log file to capture
+    # MAPQ score changes 
+    logfh = open(outlog, 'w')
+    logfh.write('readID\tbeforeMAPQ\tafterMAPQ\n')
+
+    for read in infh.fetch(until_eof=True):
+        rid = read.query_name
+        mapq = read.mapping_quality
+        if is_spliced(read):
+            # Read is spliced, compare the
+            # current read mapping quality
+            # to mapping quality to the mapq
+            # value in the spliced genome 
+            # (BAM2) lookup 
+            # print('Spliced... ', read)
+            new_mapq = max_mapq(mapq, rid, spliced_mapqs)
+        else:
+            # Read is NOT spliced, compare the
+            # current read mapping quality
+            # to mapping quality to the mapq
+            # value in the unspliced genome 
+            # (BAM1) lookup
+            # print('Unspliced.... ', read)
+            new_mapq = max_mapq(mapq, rid, unspliced_mapqs)
+
+        # Logs all MAPQ score changes,
+        # captures what the MAPQ value
+        # was before and after for a 
+        # given read ID
+        logfh.write('{}\t{}\t{}\n'.format(rid, mapq, new_mapq))
+        read.mapping_quality = new_mapq
+    
+        # Write read with fixed tags 
+        outfh.write(read)
+
+    # Close file handle
+    infh.close()
+    outfh.close()
+    logfh.close()
+
+
+if __name__ == '__main__':
+	# Call pseduo-main method
+	main()
